9d3274ecfc9d09202ae16429f01f12c96cb8b9be
Alvaro <atomocro@gmail.com>
2026-08-10 19:18:24 +0200

    Add examples of print function usage in Python
    Demonstrate various usages of the print function in Python.

---
+++ 01_basics/01_print.py
@@ -0,0 +1,15 @@
+###
+## 01_print.py
+# Aqui lo que hacemos es mirar como funciona la función print() en Python
+# Para ello hacemos diferentes pruebas con la función print() y sus parámetros
+###
+print("Hello World")
+
+print('Asi tambien funciona')
+
+print("Python", "es un lenguaje de programación", "muy popular")
+
+print("Python", "es un lenguaje de programación", "muy popular", sep = "-")
+
+print("Esto se imprime", end = " ")
+print("en la misma línea")
